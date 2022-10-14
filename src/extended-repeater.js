bb4d754f72f7d793f296eacc68e6a707e9f03102
Aliaksei Rudak <lexarudak@gmail.com>
2022-10-14 17:31:47 +0600

    feat: solve extended-repeator task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const opt = {
+    repeatTimes: 0,
+    separator: '+',
+    addition: '',
+    additionRepeatTimes: 0,
+    additionSeparator: '|'
+  }
+  if (options.repeatTimes) {
+    opt.repeatTimes = options.repeatTimes
+  }
+  if ('separator' in options) {
+    opt.separator = `${options.separator}`
+  }
+  if ('addition' in options) {
+    opt.addition = `${options.addition}`
+  }
+  if (options.additionRepeatTimes) {
+    opt.additionRepeatTimes = options.additionRepeatTimes
+  }
+  if ('additionSeparator' in options) {
+    opt.additionSeparator = `${options.additionSeparator}`
+  }
+
+
+
+  let myStr = `${str}`
+
+  'PLUS00PLUS00PLUS'
+  let addPart = opt.addition
+  const addPartArr = []
+
+  if (opt.additionRepeatTimes > 0) {
+    let i = 0
+    while (i < opt.additionRepeatTimes) {
+    addPartArr.push(addPart)
+    i++
+  }
+  addPart = addPartArr.join(`${opt.additionSeparator}`)
+  }
+  
+
+  let myNewStr = myStr + addPart
+  let answer = myNewStr
+
+  const strArr = []
+  if (opt.repeatTimes > 0) {
+    let y = 0 
+    while (y < opt.repeatTimes) {
+      strArr.push(myNewStr)
+      y++
+    }
+    answer = strArr.join(`${opt.separator}`)
+  }
+
+
+  return answer
 }
 
 module.exports = {
   repeater
 };
+
+// 'truefalse!!!false??? truefalse!!!false??? truefalse!!!false'
+// console.log(repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }));
