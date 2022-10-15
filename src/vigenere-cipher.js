c6e2baf3e09fd97d522c8780d9a8ae3f7ebec748
Aliaksei Rudak <lexarudak@gmail.com>
2022-10-15 17:12:32 +0600

    feat: solve Vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,111 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (type) {
+    this.type = type
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (arguments.length !== 2 || (typeof message !== 'string') || (typeof key !== 'string')) {
+      throw new Error('Incorrect arguments!')
+    }
+    const alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+    const allLet = alph.length
+    const messageArr = message.split('')
+    const answer = []
+    const codeName = []
+    const codeName2 = []
+   
+    
+    let i = 0
+    let u = 0
+    while (i < message.length) {
+      u = (key.length + i) % key.length
+      codeName.push(key[u = (key.length + i) % key.length].toUpperCase())
+      i++
+    }
+    let g = 0
+     messageArr.forEach((value, index,) => {
+      if (alph.includes(value.toUpperCase())) {
+        codeName2.push(codeName[g].toUpperCase())
+        g++
+      } else {
+        codeName2.push(value)
+      }
+    })
+    g = 0
+    messageArr.forEach((value, index) => {
+      if (alph.includes(value.toUpperCase()))
+      {
+        const lag = alph.indexOf(codeName2[index])
+        const base = alph.indexOf(messageArr[index].toUpperCase())
+        answer.push(alph[(allLet + (base + lag)) % allLet])
+      } else {
+        answer.push(value)
+      }
+      
+    });
+    if (this.type !== false) {
+      return answer.join('')
+    } else {
+      return answer.reverse().join('')
+    }
+    
+  }
+  decrypt(message, key) {
+    if (arguments.length !== 2 || (typeof message !== 'string') || (typeof key !== 'string')) {
+      throw new Error('Incorrect arguments!')
+    }
+    const alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+    const allLet = alph.length
+    const messageArr = message.split('')
+    const answer = []
+    const codeName = []
+    const codeName2 = []
+   
+    
+    let i = 0
+    let u = 0
+    while (i < message.length) {
+      u = (key.length + i) % key.length
+      codeName.push(key[u = (key.length + i) % key.length].toUpperCase())
+      i++
+    }
+    let g = 0
+     messageArr.forEach((value, index,) => {
+      if (alph.includes(value.toUpperCase())) {
+        codeName2.push(codeName[g].toUpperCase())
+        g++
+      } else {
+        codeName2.push(value)
+      }
+    })
+    g = 0
+    messageArr.forEach((value, index) => {
+      if (alph.includes(value.toUpperCase()))
+      {
+        const lag = alph.indexOf(codeName2[index])
+        const base = alph.indexOf(messageArr[index].toUpperCase())
+        answer.push(alph[(allLet + (base - lag)) % allLet])
+      } else {
+        answer.push(value)
+      }
+      
+    });
+    if (this.type !== false) {
+      return answer.join('')
+    } else {
+      return answer.reverse().join('')
+    }
+    
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
+
+// const directMachine = new VigenereCipheringMachine()
+// console.log(directMachine.encrypt('attack at dawn!', 'alphonse'));
+// console.log(directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse'));
+// // 'AEIHQX SX DLLU!'
