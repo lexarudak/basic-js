ec1363ee2fed9c069ccb105486aaa804d53ae284
Aliaksei Rudak <lexarudak@gmail.com>
2022-10-17 14:59:06 +0600

    feat: solve DNS-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -25,12 +25,35 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getDNSStats(domains) {
   let answer = {}
   const arr = []
-  const arr2 = []
+  const newArr = []
+
+
   domains.forEach(val => {
     arr.push(val.split('.').reverse())
   })
+  
   console.log(arr);
 
+   arr.forEach(value => {
+     let a = []
+      value.forEach((val, ind, ar) => {
+    if (ind === 0){
+      a.push('.' + val)
+    } else {
+      a.push(a[ind - 1] + '.' + val)
+    }
+  })
+    newArr.push(...a)
+  })
+
+  console.log('newArr', newArr);
+  newArr.forEach(value => {
+    if (!answer[value]) {
+      answer[value] = 1
+    } else {
+      answer[value]++
+    }
+  })
   return answer
 }
 
@@ -38,4 +61,4 @@ module.exports = {
   getDNSStats
 };
 
-console.log(getDNSStats(['epam.com', 'info.epam.com']));
+// console.log(getDNSStats(['epam.com', 'info.epam.com']));
