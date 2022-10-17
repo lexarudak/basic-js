6b006961567dd526e6322da4abc67a8af42c9bfb
Aliaksei Rudak <lexarudak@gmail.com>
2022-10-17 16:53:03 +0600

    feat: solve recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,59 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
+  constructor () {
+    this.count = 1
+    this.toShow = 0
+  }
   calculateDepth(arr) {
-  //     throw new NotImplementedError('Not implemented');
-  // // remove line with error and write your code here
-    if (Array.isArray(arr)){
-      let finalCount = 1
-      let count = 1
-      
 
-      function fe (element) {
-        const isArr = element.filter(val => Array.isArray(val))
-        console.log('is arr:', isArr);
-        
-        if (isArr.length > 0) {
-          count ++
-          console.log('count', count);
-          
-        element.forEach((value) => {  
-          if (Array.isArray(value)){
-            console.log('VALUE:', value);
-            
-            fe(value)
-          }
+    let isArr = arr.filter(val => Array.isArray(val))
+
+    if (isArr.length > 0) {
+        this.toShow = 0
+        this.count++
+    let newArr = []  
+
+    isArr.forEach(isArrItem => {
+      isArrItem.forEach(itemInside => {
+      newArr.push(itemInside)
+          })
         })
+      this.calculateDepth(newArr)
       } else {
-        console.log('to final count', count, finalCount);
-        
-        if (count > finalCount) {
-          finalCount = count
-          count = 2
-          console.log('to final count', count, finalCount);
-          console.log('---------------------------------');
-        } else {
-          count = 2
-          console.log('to final count', count, finalCount);
-          console.log('---------------------------------');
-        }
-        
-      }
-      }
-      fe(arr)
-      
-      console.log('end', finalCount);
-      
-      return finalCount
-    } else {
-      return 0
+        console.log('to final count', this.count);
+        this.toShow = this.count
+        this.count = 1
     }
-    
+    return this.toShow
   }
 }
 
 // const depthCalc = new DepthCalculator()
-// depthCalc.calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]])
+// depthCalc.calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]])
 
 
 module.exports = {
